959d71f0f54cd962b2cf2786bb7009caf1afdf32
Kyle Hogan <klhogan@bu.edu>
2015-06-18 17:18:19 -0400

    redirect working

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+var shelljs = require('shelljs');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 router.get('/sliceDrop', function(req, res, next) {
-  res.render('sliceDrop');
+	var view = fs.open('./uploads/10_3T_nody_001.nii.gz' , r, callback);
+	console.log(view);
+	res.render('slicedrop', {view: view});
 });
 
-router.get('/ns-viewer', function(req, res, next) {
-  res.render('ns-viewer');
+router.get('/users', function(req, res, next) {
+	var view = fs.open('./uploads/10_3T_nody_001.nii.gz' , 'r');
+	console.log(fs.read(view));
+	res.render('index', {view: view});
 });
 
 router.get('/file-upload', function(req, res, next) {
-  res.render('file-upload', { title: 'Express' });
+	res.render('file-upload', { title: 'Dropzone' });
 });
 
 router.post('/file-upload', function(req, res, next) {
-    console.log(req.body);
-    console.log(req.files);
+	console.log(req.files);
+	res.send('test');
 });
 
 module.exports = router;
