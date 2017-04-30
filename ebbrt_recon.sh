a67e1308ba92c7f083545b415084234bfce0f30c
Javier Arguello <jas91@bu.edu>
2017-04-30 15:11:28 -0400

    fix(ER): now working with latest fetalRecon app

--- ebbrt_recon.sh
+++ ebbrt_recon.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+
 export RECON_DIR=~/EbbRT-fetalRecon
 export RECONCMD=${RECON_DIR}/build/reconstruction
-
 
 tranid=$1
 threads=$(($2+1))
 iterations=$3
 backends=$4
 mask=$5
+frontEndCpus=$(($backends+1))
 
 uploads="./uploads"
 trandir="./transactions/${tranid}"
 
+export EBBRT_NODE_ALLOCATOR_DEFAULT_CPUS=$threads
+export EBBRT_NODE_ALLOCATOR_DEFAULT_RAM=4
+export EBBRT_NODE_ALLOCATOR_DEFAULT_NUMANODES=1
+
 function doRecon() {
-    echo ${trandir}
     mkdir -p ${trandir}/slices ${trandir}/mask
     mv ${uploads}/* ${trandir}/slices
 
@@ -25,14 +29,17 @@ function doRecon() {
         MASK="-m ${trandir}/mask/${mask}"
     fi
 
-    ${RECONCMD} -o ${trandir}/${tranid}.nii \
+    cmd="${RECONCMD} -o ${trandir}/${tranid}.nii \
     -i ${trandir}/slices/*.nii \
     ${MASK} \
     --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 2.0 \
     --debug 1 \
     --numThreads $threads --useCPU \
     --iterations $iterations --numNodes $backends \
-    --numFrontEndCpus $(($backends+1))
+    --numFrontEndCpus ${frontEndCpus}"
+
+    echo ${cmd}
+    $cmd
     
     cp ${trandir}/${tranid}.nii public/recon/
 }
